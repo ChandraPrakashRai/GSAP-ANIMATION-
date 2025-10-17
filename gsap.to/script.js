c824db134ff374fc5d843d666ba824e843f2e624
Chandra Prakash Rai <crai60433@gmail.com>
2025-10-17 18:49:39 +0530

    gsa.to() , gsa.from()

---
+++ gsap.to/script.js
@@ -0,0 +1,16 @@
+//  document.addEventListener("DOMContentLoaded", (event) => {
+//   // gsap code here!
+//     gsap.to("#child1" , {
+//         top : "1000px" ,
+//         duration : 5,
+        
+//     })
+//  });
+ document.addEventListener("DOMContentLoaded", (event) => {
+  // gsap code here!
+    gsap.from("#child1" , {
+        left : "100px" ,
+        duration : 5,
+        
+    })
+ });
